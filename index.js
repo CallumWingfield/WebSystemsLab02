40ce2f7245b4010894e764d20f59a8aae99813b8
callum <C-W>
2022-02-05 16:52:49 +0000

    error handle

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ response.writeHead(200, {'Content-Type': 'text/plain'});
 response.end('Hello World\n');
 }).listen(PORT);
 
-
-app.listen(PORT, () => {
- console.log(`Our app is running on port ${ PORT }`);
-});
-
-
 console.log('Server running at http://127.0.0.1:' + PORT);
 
 
